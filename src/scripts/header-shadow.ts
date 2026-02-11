953be39a3ff9a763866492f2947d02949eb744b3
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-02-11 15:44:21 -0500

    Add header shadow effect

---
+++ src/scripts/header-shadow.ts
@@ -0,0 +1,8 @@
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  globalThis.scrollY >= 50
+    ? header?.classList.add("shadow-header")
+    : header?.classList.remove("shadow-header");
+};
+
+window.addEventListener("scroll" ,shadowHeader)
