14e452b1395799768c6712acdb955e2123b55c92
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-02-12 15:10:37 -0500

    create projects constant

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+import type { Project } from "../types";
+
+export const projects: Project[] = [
+  {
+    name: "ThePromptBook",
+    image: "img/thepromptbook.png",
+    description:
+      "A web application that allows users to create and share prompts for AI models.",
+    href: "https://www.thepromptbook.cloud",
+  },
+];
