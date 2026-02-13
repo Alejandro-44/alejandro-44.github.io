23add1f260bc0fae204b568009dd55c8e8814f00
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-02-13 16:50:50 -0500

    Add dynamic typing animation to the home page using `typed.js`.

---
+++ src/scripts/typed.ts
@@ -0,0 +1,10 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#typed", {
+    strings: ["Ingeniero de Sistemas", "Desarrollador Web", "Frontend Developer", "Backend Developer", "Fullstack Developer"],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: "_"
+})
